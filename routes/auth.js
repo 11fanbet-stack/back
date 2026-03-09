53bc5d5deb23f9cc5d0ebc1ab453be298ffd0f67
11fanbet-stack <11fanbet@gmail.com>
2026-03-09 18:49:59 +0530

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const router = require("express").Router()
+
+const User = require("../models/User")
+
+router.post("/register", async (req,res)=>{
+
+const {username,email,password} = req.body
+
+try{
+
+const existingUser = await User.findOne({email})
+
+if(existingUser){
+
+return res.status(400).json({
+message:"User already exists"
+})
+
+}
+
+const newUser = new User({
+username,
+email,
+password
+})
+
+await newUser.save()
+
+res.json({
+message:"User Registered Successfully"
+})
+
+}catch(error){
+
+res.status(500).json({
+message:"Server Error"
+})
+
+}
+
+})
+
+module.exports = router
